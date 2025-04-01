891cdebb8af151f2b153721bb732707135b24216
NickGM <lknick23@gmail.com>
2025-04-01 20:40:37 +0300

    finished routing users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,4 @@
-//import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/libs/prisma';
-import { redirect } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
@@ -9,16 +7,12 @@ export async function GET() {
   return NextResponse.json(users);
 }
 
-//export async function GET(request: Request) {
-//  redirect('https://google.com/');
-//}
+export async function POST(req: NextRequest) {
+  const data = await req.json();
 
-// export async function POST(req: NextRequest) {
-//   const data = await req.json();
+  const user = await prisma.user.create({
+    data,
+  });
 
-//   const user = await prisma.user.create({
-//     data,
-//   });
-
-//   return NextResponse.json(user);
-// }
+  return NextResponse.json(user);
+}
