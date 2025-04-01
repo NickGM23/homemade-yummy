dddf34fb31706aa3a471a8c3944cefe75f07a1ad
NickGM <lknick23@gmail.com>
2025-04-01 19:51:50 +0300

    test3

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,24 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '@/prisma/prisma';
-
+import { prisma } from '@/libs/prisma';
 import { redirect } from 'next/navigation';
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
+}
 
 //export async function GET(request: Request) {
 //  redirect('https://google.com/');
 //}
 
-//export async function GET() {
-//  const users = await prisma.user.findMany();
-//
-//  return NextResponse.json(users);
-//}
+// export async function POST(req: NextRequest) {
+//   const data = await req.json();
 
-export async function POST(req: NextRequest) {
-  const data = await req.json();
+//   const user = await prisma.user.create({
+//     data,
+//   });
 
-  const user = await prisma.user.create({
-    data,
-  });
-
-  return NextResponse.json(user);
-}
+//   return NextResponse.json(user);
+// }
