08346d3948d3e3e2756bf43b45d27d8346166638
NickGM <lknick23@gmail.com>
2025-10-20 13:27:10 +0300

    update prisma.ts

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    // log: ['query', 'info', 'warn', 'error'],
+
+    log: [],
+  });
 };
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+export const prisma = prismaClientSingleton();
