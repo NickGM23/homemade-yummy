c9362d82778dacb9b1d9ee1f6b094b4eabfcf175
NickGM <lknick23@gmail.com>
2025-11-28 21:16:20 +0200

    fixed infoblock

--- components/shared/info-block.tsx
+++ components/shared/info-block.tsx
@@ -14,29 +14,38 @@ interface Props {
 
 export const InfoBlock: React.FC<Props> = ({ className, title, text, imageUrl }) => {
   return (
-    <div className={cn(className, 'flex w-[840px] items-center justify-between gap-12')}>
-      <div className="flex flex-col">
-        <div className="w-[445px]">
-          <Title size="lg" text={title} className="font-extrabold" />
-          <p className="text-lg text-gray-400">{text}</p>
-        </div>
+    <div
+      className={cn(
+        className,
+        `flex flex-col items-center gap-6 px-4 text-center md:w-[840px] md:flex-row md:items-start md:justify-between md:gap-12 md:text-left`,
+      )}
+    >
+      {/* TEXT BLOCK */}
+      <div className="flex max-w-full flex-col md:max-w-[445px]">
+        <Title size="lg" text={title} className="font-extrabold" />
+        <p className="mt-2 text-base text-gray-400 md:text-lg">{text}</p>
 
-        <div className="mt-11 flex gap-5">
+        <div className="mt-6 flex flex-col gap-4 md:mt-11 md:flex-row md:gap-5">
           <Link href="/">
-            <Button variant="outline" className="gap-2">
+            <Button variant="outline" className="w-full gap-2 md:w-auto">
               <ArrowLeft />
               На головну
             </Button>
           </Link>
-          <a href="">
-            <Button variant="outline" className="border-gray-400 text-gray-500 hover:bg-gray-50">
+
+          <button className="w-full md:w-auto">
+            <Button
+              variant="outline"
+              className="w-full border-gray-400 text-gray-500 hover:bg-gray-50 md:w-auto"
+            >
               Оновити
             </Button>
-          </a>
+          </button>
         </div>
       </div>
 
-      <img src={imageUrl} alt={title} width={300} />
+      {/* IMAGE */}
+      {imageUrl && <img src={imageUrl} alt={title} className="h-auto w-40 md:w-[300px]" />}
     </div>
   );
 };
